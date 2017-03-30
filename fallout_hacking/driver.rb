52471a1fdee065dd0cc095091241422a58c94d9b
Ed Woodfall <Edward.Woodfall@simplybusiness.co.uk>
2017-03-30 13:44:34 +0100

    functioning game

--- fallout_hacking/driver.rb
+++ fallout_hacking/driver.rb
@@ -7,17 +7,24 @@ wordlist = File.read('./wordlist.txt').split
 
 f = Fallout.new(complexity, wordlist)
 f.display
+puts f.refined_list
 
 i = 1
 num = 5
 
 while i < num  do
-   puts("Enter your guess: #{i} of #{num}" )
-   guess = gets
+	puts("Enter your guess: #{i} of #{num-1}" )
+    guess = gets.gsub("\n",'')
+
+	until f.refined_list.include?(guess) do
+   	  puts("Not in list: Enter your guess: #{i} of #{num-1}" )
+      guess = gets
+    end
 
    result = f.evaluate_guess(guess)
+   puts result
 
-   return 'you won' result == 
+   break 'you won' if result["result"] 
 
 	i += 1
     puts 'you lost'
--- fallout_hacking/fallout.rb
+++ fallout_hacking/fallout.rb
@@ -1,16 +1,31 @@
 require 'securerandom'
 
 class Fallout  
+
+  attr_reader :refined_list
+
   def initialize(complexity = 1, wordlist = ['hello', 'world'])  
  
     @complexity = set_complexity(complexity)
     @wordlist = wordlist  
+    refine_wordlist complexity
     set_word
   end  
 
+  def refine_wordlist(complexity)
+
+  	complexity_match = {'1' => 4..6, '2' => 7..9, '3' => 10..12, '4' => 13..14, '5' => 15 }
+  	number_range = complexity_match[complexity.to_s]
+  	
+  	@refined_list = @wordlist.select { |word|
+  		number_range.include?(word.length)
+  	 }.sample(15)
+
+  end
+
   def set_word
-  	word_index = SecureRandom.random_number @wordlist.length
-  	@current_word = @wordlist[word_index]
+  	word_index = SecureRandom.random_number @refined_list.length
+  	@current_word = @refined_list[word_index]
   	@split_word = @current_word.split('')
   end
 
@@ -28,14 +43,13 @@ class Fallout
   end  
 
   def evaluate_guess(guess = '')
+  	#require 'pry'
+  	#binding.pry
 
   	w1 = guess.split('')
-  	result = w1.zip(@split_word).map { |x, y| x == y }
+  	result = @split_word.zip(w1).map { |x, y| x == y }
 
-  	grades = {"correct" =>  result.count(true), "incorrect" => result.count(false)}
-
-
-
+  	{"result" =>  result.all?, "correct" =>  result.count(true), "incorrect" => result.count(false) }
 
   end
 
